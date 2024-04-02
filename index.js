3509befa1c38275a097ea22e725722a88722bd67
Khalil BH <149834524+khalilBenHmida@users.noreply.github.com>
2024-04-02 15:27:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api/auth', authController)
 app.use('/api/blog', blogController)
+app.use('/api/test', (req, res) => {
+    res.send("Hello routing")
+})
 
 // multer
 const storage = multer.diskStorage({

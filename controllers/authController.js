00cf126f384acf1b67d8667390a5691768676f84
khalilBenHmida <khalilbusiness1000@gmail.com>
2024-04-02 13:11:41 +0100

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,44 @@
+const authController = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+authController.post("/register", async (req, res) => {
+  try {
+    const isExisting = await User.findOne({ email: req.body.email });
+    if (isExisting) {
+      throw new Error("Already such an account. Try a different email");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    const { password, ...others } = newUser._doc;
+    res.status(201).json(newUser);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+authController.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const comparePass = await bcrypt.compare(req.body.password, user.password);
+    if (!comparePass) {
+      throw new error("Invalid credentials");
+    }
+    const { password, ...others } = user._doc;
+    const token = jwt.sign({ id: user._id }, process.env.JWT_secret, {
+      expiresIn: "5h",
+    });
+    return res.status(200).json({ user: others, token });
+  } catch (error) {
+     console.log(error)
+    return res.status(500).json(error);
+  }
+});
+module.exports = authController;
